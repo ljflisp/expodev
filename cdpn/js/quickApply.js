168f7f6ee4b00829a28c122cdb839c45675071e0
Andy Woz <159766349+wozandy05@users.noreply.github.com>
2025-04-24 01:05:24 +0000

    Add quickApply.js

---
+++ cdpn/js/quickApply.js
@@ -0,0 +1,17 @@
+function quickApply(str, fn, ele, options={}) {
+  const {insert=false, pos='afterbegin'} = options
+  const data = str.split(' ').map(s => s.split('|'))
+  if (data[0].length == 1) data.flat()
+  const Z = (fn) => data.map(fn).join('')
+  if (ele == 'style') {
+    let style = document.createElement('style')
+    style.innerHTML = Z(fn)
+    document.head.appendChild(style)
+  } else {
+    if (insert) {
+      document.querySelector(ele).insertAdjacentHTML(pos, Z(fn))
+    } else {
+        document.querySelector(ele).innerHTML = Z(fn)
+    }
+  }
+}
