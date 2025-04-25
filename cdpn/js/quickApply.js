c55350114df3eb4a58d414c6268fbfcadfa66707
Andy Woz <159766349+wozandy05@users.noreply.github.com>
2025-04-25 02:02:36 +0000

    Add styleAnim obj to destructing in quickApply.js

--- cdpn/js/quickApply.js
+++ cdpn/js/quickApply.js
@@ -1,11 +1,11 @@
 function quickApply(str, fn, ele, options={}) {
-  const {insert=false, pos='afterbegin'} = options
+  const {insert=false, pos='afterbegin', styleAnim: {toStr=false,animName='anim'}={}} = options
   const data = str.split(' ').map(s => s.split('|'))
   if (data[0].length == 1) data.flat()
   const Z = (fn) => data.map(fn).join('')
   if (ele == 'style') {
     let style = document.createElement('style')
-    style.innerHTML = Z(fn)
+    style.innerHTML = toStr ? `@keyframes ${animName}{`+Z(fn)+'}' : Z(fn)
     document.head.appendChild(style)
   } else {
     if (insert) {
