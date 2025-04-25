6a07860da345b6b86d4d92ef2b068d786d44dd37
Andy Woz <159766349+wozandy05@users.noreply.github.com>
2025-04-25 06:01:47 +0000

    Add attr(styleId) to obj(style) and function quickQuery

--- cdpn/js/quickApply.js
+++ cdpn/js/quickApply.js
@@ -1,12 +1,20 @@
 function quickApply(str, fn, ele, options={}) {
-  const {insert=false, pos='afterbegin', styleAnim: {toStr=false,animName='anim'}={}} = options
+  const {insert=false, pos='afterbegin', style: {isAnim=false, animName='anim', styleId='my-style'}={}} = options
   let data = str.split(' ')
   if (/\|/.test(str)) data = data.map(s => s.split('|'))
   const Z = (fn) => data.map(fn).join('')
   if (ele == 'style') {
-    let style = document.createElement('style')
-    style.innerHTML = toStr ? `@keyframes ${animName}{`+Z(fn)+'}' : Z(fn)
-    document.head.appendChild(style)
+    const existStyle = document.querySelector('style#'+styleId)
+    let cssRuleStr = isAnim ? `@keyframes ${animName}{`+Z(fn)+'}' : Z(fn)
+
+    if (existStyle) {
+      existStyle.innerHTML += cssRuleStr
+    } else {
+      let style = document.createElement('style')
+      style.id = styleId
+      style.innerHTML = cssRuleStr
+      document.head.appendChild(style)
+    }
   } else {
     if (insert) {
       document.querySelector(ele).insertAdjacentHTML(pos, Z(fn))
@@ -14,4 +22,8 @@ function quickApply(str, fn, ele, options={}) {
         document.querySelector(ele).innerHTML = Z(fn)
     }
   }
+}
+
+function quickQuery(str, sign='', separator=' ') {
+  return str.split(separator).map(s => document.querySelector(sign+s))
 }
