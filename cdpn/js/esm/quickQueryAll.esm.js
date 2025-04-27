41ffe888adce1b3c6923d7e92d08ee90b286e4cc
Andy Woz <159766349+wozandy05@users.noreply.github.com>
2025-04-27 02:39:43 +0000

    feat: add insertHtml function to quickQueryAll.esm.js
    - Helps simplify DOM manipulation tasks.
    - New function insertHtml to dynamically insert HTML.
    - Supports setting tag name, insert position, parent selector, and optional processing function.

--- cdpn/js/esm/quickQueryAll.esm.js
+++ cdpn/js/esm/quickQueryAll.esm.js
@@ -1,6 +1,15 @@
 var queryAll = (element) => document.querySelectorAll(element);
 var documentEvent = (evt, fn) => document.addEventListener(evt, fn);
+var insertHtml = (options) => {
+  const { str, label, pos = "afterbegin", parent = "body", fn, isNormal = false } = options || {};
+  let data = str.split(" ");
+  if (/\|/.test(str))
+    data = data.map((s) => s.split("|"));
+  const Z = (fn2) => data.map(fn2).join("");
+  document.querySelector(parent)?.insertAdjacentHTML(pos, isNormal ? str : `<${label}>${Z(fn)}</${label}>`);
+};
 export {
   queryAll,
+  insertHtml,
   documentEvent
 };
